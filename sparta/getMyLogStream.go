22739a38cf65447679ec25e4cccdc289dcf710fc
michaelzhang <heronalps@gmail.com>
2017-05-06 12:25:31 -0700

    Finish getMyLogStream.go

--- sparta/getMyLogStream.go
+++ sparta/getMyLogStream.go
@@ -4,14 +4,64 @@ import (
   "encoding/json"
   "fmt"
   "net/http"
-  "github.com/Sirupsen/logrus"
+  "os"
+  "strconv"
+  logrus "github.com/Sirupsen/logrus"
   sparta "github.com/mweagle/Sparta"
 )
+
+func init() {
+  // Log as JSON instead of the default ASCII formatter.
+  logrus.SetFormatter(&logrus.JSONFormatter{})
+
+  // Output to stdout instead of the default stderr
+  // Can be any io.Writer, see below for File example
+  logrus.SetOutput(os.Stdout)
+
+  // Only log the warning severity or above.
+  logrus.SetLevel(logrus.InfoLevel)
+}
+
+func dosomething(){
+    var m map[string]int = make(map[string]int)
+    for i := 0; ; i++{
+        s := strconv.Itoa(i)
+        m["name" + s] = i
+        fmt.Println(m)
+    }
+}
+
+type MyError string
+
+func (e MyError) Error() string{
+	return fmt.Sprintf("My Error : %s", e)
+}
 
 func getMyLogStream(event *json.RawMessage,
                     context *sparta.LambdaContext,
                     w http.ResponseWriter,
                     logger *logrus.Logger) {
+
+    // Insert ill-behaved instructions to test AWS Lambda limits
+    // Infinite loop
+    /*
+    for {
+        fmt.Println(event)
+    }
+    */
+    //Memory Leak
+    /*
+    for {
+        go dosomething()
+    }
+    */
+
+    //Exit with error
+    /*
+    panic("Stop the lambda function") //Panic exit
+    os.Exit(1) // OS Exit
+    fmt.Fprintf(os.Stderr, "error: %v \n ", MyError("Intentional Error")) // Fatal error : Stack overflow
+    */
 
     fmt.Fprintf(w, "%s  ", "This Go function is to get AWS Lambda function log stream! ")
     fmt.Fprintf(w, "%s : %s  ", "AWSRequestID", context.AWSRequestID)
@@ -21,6 +71,16 @@ func getMyLogStream(event *json.RawMessage,
     fmt.Fprintf(w, "%s : %s  ", "MemoryLimitInMB", context.MemoryLimitInMB)
     fmt.Fprintf(w, "%s : %s  ", "FunctionVersion", context.FunctionVersion)
     fmt.Fprintf(w, "%s : %s  ", "InvokedFunctionARN", context.InvokedFunctionARN)
+
+    logrus.WithFields(logrus.Fields{
+      "AWSRequestID" : context.AWSRequestID,
+      "InvokeID" : context.InvokeID,
+      "LogGroupName" : context.LogGroupName,
+      "FunctionName" : context.FunctionName,
+      "MemoryLimitInMB" : context.MemoryLimitInMB,
+      "FunctionVersion" : context.FunctionVersion,
+      "InvokedFunctionARN" : context.InvokedFunctionARN,
+    }).Info("All runtime information from AWS Lambda context.")
 }
 
 func main() {

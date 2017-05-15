7bb32c066a9c3b2129d02f0b47dc67bfda9e9c14
michaelzhang <heronalps@gmail.com>
2017-05-15 01:13:47 -0700

    Remove rds_config

--- Latency_Experiment/Python/rds_config.py
+++
@@ -1,5 +0,0 @@
-#config file containing credentials for rds mysql instance
-db_username = "michael"
-db_password = "qwerty46"
-db_name = "myLatencyExperimentDB"
-db_endpoint = "latencyexperiment.cxfpxjlukepw.us-west-2.rds.amazonaws.com"
